601d67effdd7ef36a2cf360ee1c0754b3799090e
Onrwir <115116487+Onrwir@users.noreply.github.com>
2023-08-28 22:07:49 +0200

    Added granting permission for External storage

--- app/src/main/java/pl/onrwir/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/pl/onrwir/kidsdrawingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package pl.onrwir.kidsdrawingapp
 
+import android.app.Activity
 import android.app.Dialog
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +10,10 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
@@ -16,21 +22,51 @@ class MainActivity : AppCompatActivity() {
     private var drawingView : DrawingView? = null
     private var mImageButtonCurrentPaint : ImageButton? = null
 
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted){
+                    Toast.makeText(
+                        this,
+                        "Permission granted, now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }else{
+                    if (permissionName==android.Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(
+                            this,
+                            "Permission denied.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
-        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
         mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
         mImageButtonCurrentPaint!!.setImageDrawable(
             ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showBrushSizeChoserDialog()
+        }
+
+        val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener{
+            requestStoragePermission()
         }
 
     }
@@ -70,8 +106,86 @@ class MainActivity : AppCompatActivity() {
                 ContextCompat.getDrawable(this, R.drawable.pallet_normal)
             )
             mImageButtonCurrentPaint = view
+        }
+    }
+
+    private fun requestStoragePermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+
+                Toast.makeText(
+                    this,
+                    "Permission granted, now you can read the storage files.",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                //Permisssion is granted
+//                requestPermission.launch(
+//                arrayOf(
+//                android.Manifest.permission.READ_EXTERNAL_STORAGE
+//            // TODO - Add writing external storage permission
+//                    )
+//                )
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                //Additional rationale should be displayed
+
+                showRationalDialog("Kids Drawing App", "Kids Drawing App needs to Access Your External Storage")
+
+            }
+            else -> {
+                //Permission has not been asked yet
+                requestPermission.launch(
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    // TODO - Add writing external storage permission
+                    )
+                )
+            }
 
 
         }
+//        if (ActivityCompat.shouldShowRequestPermissionRationale(
+//                this,
+//                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+//        ){
+//            showRationalDialog("Kids Drawing App", "Kids Drawing App needs to Access Your External Storage")
+//        }else{
+//            requestPermission.launch(
+//                arrayOf(
+//                android.Manifest.permission.READ_EXTERNAL_STORAGE
+//            // TODO - Add writing external storage permission
+//                )
+//            )
+//        }
+    }
+
+    private fun showRationalDialog(
+        title: String,
+        message: String
+    ){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder. setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK"){dialog, _ ->
+                requestPermission.launch(
+                    arrayOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                        // TODO - Add writing external storage permission
+                    )
+                )
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }
+
+        builder.create().show()
     }
 }
