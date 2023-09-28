fff76f4138debbbee65762909db3e93f48f5cc32
Onrwir <115116487+Onrwir@users.noreply.github.com>
2023-09-28 19:50:43 +0200

    Added Progress bar when saving the image

--- app/src/main/java/pl/onrwir/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/pl/onrwir/kidsdrawingapp/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView : DrawingView? = null
     private var mImageButtonCurrentPaint : ImageButton? = null
+    var customProgressDialog : Dialog? = null
 
     val openGalleryLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
@@ -93,6 +94,7 @@ class MainActivity : AppCompatActivity() {
         val ibSave : ImageButton = findViewById(R.id.ib_save)
         ibSave.setOnClickListener {
             if(isReadStorageAllowed()){
+                showProgressDialog()
                 lifecycleScope.launch {
                     val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
                     saveBitmapFile(getBitmapFromView(flDrawingView))
@@ -227,6 +229,7 @@ class MainActivity : AppCompatActivity() {
                     
                     result = f.absolutePath
                     runOnUiThread {
+                        cancelProgressDialog()
                         if(result.isNotEmpty()){
                             Toast.makeText(this@MainActivity,
                                 "File saved successfully: $result",
@@ -271,5 +274,18 @@ class MainActivity : AppCompatActivity() {
             }
 
         builder.create().show()
+    }
+
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this@MainActivity)
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog?.show()
+    }
+
+    private fun cancelProgressDialog(){
+        if(customProgressDialog != null){
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
     }
 }
